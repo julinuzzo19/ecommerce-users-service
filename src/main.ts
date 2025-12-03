37db8a659c4fc463180c2a467f02723404719880
julinuzzo19 <julinuzzo19@gmail.com>
2025-12-03 06:09:29 +0000

    feat: update package.json and main.ts for improved configuration and security
    - Changed script name from "start:dev" to "dev" for consistency.
    - Added new dependencies: @nestjs/jwt, class-validator, helmet, and multer.
    - Added @types/multer for TypeScript support.
    - Enhanced main.ts to include helmet for security, CORS configuration, and static file serving for avatars.
    - Set global API prefix to 'api/v1' and configured the server to listen on a dynamic port.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  // helmet configuration
+  app.use(helmet());
+
+  // CORS configuration
+  app.enableCors({
+    methods: ['POST', 'GET', 'PUT', 'PATCH', 'DELETE'],
+    origin:
+      configService.get<string>('NODE_ENV') === 'production'
+        ? configService.get<string>('CLIENT_URL')
+        : '*',
+    credentials: true,
+  });
+
+  // add prefix
+  app.setGlobalPrefix('api/v1');
+
+  // serve avatar users
+  app.use('/avatars', express.static(join(process.cwd(), 'public', 'avatars')));
+
+  await app.listen(configService.get<number>('PORT') || 3001);
 }
 bootstrap();
